26f24313ea5f4482b8da8b8815300049ba5a8c37
Prashant Verma <test_value123>
2021-04-09 10:27:19 +0530

    included fix for the decorator

---
+++ src/js/components/decorator/Person.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+/**
+ * ? Even if this function returns nothing this will be rendering the old component
+ * @param {*} WrappedComponent
+ * @param {*} key
+ * @param {*} descriptor
+ */
+function enable(WrappedComponent, key, descriptor) {
+  console.log("Decorator called!!!", WrappedComponent);
+  WrappedComponent.lips = "pink";
+
+  //! below gives error because this is the we are trying to instantiate and return the component here
+  // return <>{target}</>;
+  // return (
+  //   <>
+  //     <h1>Wrapped in Decorator</h1>
+  //     <WrappedComponent />
+  //   </>
+  // );
+
+  //! below gives error because of the naming convention
+  // return (props) => {
+  //   return <target />;
+  // };
+
+  //! cannt call class as the function
+  // return <>{WrappedComponent()}</>;
+
+  return (props) => {
+    return (
+      <>
+        <h1>Wrapped in the Decorator</h1>
+        <WrappedComponent />
+      </>
+    );
+  };
+}
+
+/**
+ * for removing the errors in the visual code for this Decorator function
+ * we have to set the compiler options in the jsconfig.json
+ */
+@enable
+export default class Person extends Component {
+  render() {
+    return <span>Sample example for the Decorator.</span>;
+  }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -4,17 +4,25 @@
  */
 import React from "react";
 import App from "./App.js";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import PageNotFound from "./components/content/PageNotFound.js";
 import HOCDemo from "./components/higher-order-components/HigherOrderComponentDemo.js";
 import Header from "./components/header/Header.js";
 import Footer from "./components/footer/Footer.js";
 import RefDemo from "./components/content/RefDemo.js";
 import PureVsImpureDemo from "./components/content/PureVsImpureComponent.js";
-import RouterExamples from './components/router-functionalities';
-import Navigation from './components/navigation/Navigation';
+import RouterExamples from "./components/router-functionalities";
+import Navigation from "./components/navigation/Navigation";
 
-import { generateNavLinksFromConfig, generateRoutesFromConfig } from './utils/utils';
+import {
+  generateNavLinksFromConfig,
+  generateRoutesFromConfig,
+} from "./utils/utils";
 import HooksDemo from "./components/hooks/HooksDemo.js";
 import CodeSplitExample from "./components/content/CodeSplitExample.js";
 import Demo from "./components/content/FunctionalComponent.js";
@@ -24,38 +32,58 @@ import NoHtmlDemo from "./components/content/NoHtml.js";
 import ContextDemo from "./components/context/index.js";
 import MemoDemo from "./components/memo/index.js";
 import LazyLoadDemo from "./components/lazy/index.js";
+import Person from "./components/decorator/Person";
 
 /**
  * Configuration information for the main navigation.
  * This will be iterated over to create the navigation.
  */
 const mainNavigationConfig = [
-  { path: '/', name: 'App', component: App },
-  { path: '/memo-demo', name: 'Memo Demo', component: MemoDemo },
-  { path: '/lazy-demo', name: 'LazyLoad Demo', component: LazyLoadDemo },
-  { path: '/demo', name: 'Functional Component', component: Demo },
-  { path: '/context', name: 'Context API', component: ContextDemo },
-  { path: '/orientation', name: 'Orientation Checker', component: WindowDimensions },
-  { path: '/without-render', name: 'react without render', component: NoHtmlDemo },
-  { path: '/danger', name: 'Danger Component', component: DangerHtml },
-  { path: '/hoc', name: 'High Order Component (HOC)', component: HOCDemo },
-  { path: '/ref', name: 'Ref. Usage', component: RefDemo },
-  { path: '/pure-vs-impure', name: 'Pure Vs Impure Component', render: (props) => { return <PureVsImpureDemo customProps="hi" />; } },
-  { path: '/router-examples', name: 'Router Usage Examples' },
-  { path: '/hooks', name: 'Hooks' },
-  { path: "/code-splitting", name: "Code Splitting", component: CodeSplitExample },
+  { path: "/", name: "App", component: App },
+  { path: "/deco-demo", name: "Decorator Demo", component: Person },
+  { path: "/memo-demo", name: "Memo Demo", component: MemoDemo },
+  { path: "/lazy-demo", name: "LazyLoad Demo", component: LazyLoadDemo },
+  { path: "/demo", name: "Functional Component", component: Demo },
+  { path: "/context", name: "Context API", component: ContextDemo },
+  {
+    path: "/orientation",
+    name: "Orientation Checker",
+    component: WindowDimensions,
+  },
+  {
+    path: "/without-render",
+    name: "react without render",
+    component: NoHtmlDemo,
+  },
+  { path: "/danger", name: "Danger Component", component: DangerHtml },
+  { path: "/hoc", name: "High Order Component (HOC)", component: HOCDemo },
+  { path: "/ref", name: "Ref. Usage", component: RefDemo },
+  {
+    path: "/pure-vs-impure",
+    name: "Pure Vs Impure Component",
+    render: (props) => {
+      return <PureVsImpureDemo customProps="hi" />;
+    },
+  },
+  { path: "/router-examples", name: "Router Usage Examples" },
+  { path: "/hooks", name: "Hooks" },
+  {
+    path: "/code-splitting",
+    name: "Code Splitting",
+    component: CodeSplitExample,
+  },
   { path: "/relative", name: "Using webpack for relative path" },
-  { path: '/ssr', name: 'Server side rendering' },
-  { path: '/decorator', name: 'Decorator Component' },
-  { path: '/memo', name: 'Memoize Component' },
-  { path: '/scroll-height', name: 'Scroll height restoration Component' },
-  { path: '/set-state-callback', name: 'set state callback' },
-  { path: '/prop-types', name: 'PropTypes demo Shape' },
-]
+  { path: "/ssr", name: "Server side rendering" },
+  { path: "/decorator", name: "Decorator Component" },
+  { path: "/memo", name: "Memoize Component" },
+  { path: "/scroll-height", name: "Scroll height restoration Component" },
+  { path: "/set-state-callback", name: "set state callback" },
+  { path: "/prop-types", name: "PropTypes demo Shape" },
+];
 
 /**
- * Main Component just returning Router 
- * @param {*} props 
+ * Main Component just returning Router
+ * @param {*} props
  */
 export const Main = function (props) {
   /* @todo : using the configuration and generating the routes is needed now. */
@@ -71,6 +99,7 @@ export const Main = function (props) {
           <Route path="/ref">
             <RefDemo />
           </Route>
+          <Route path="/deco-demo" component={Person} />
           <Route path="/memo-demo" component={MemoDemo} />
           <Route path="/lazy-demo" component={LazyLoadDemo} />
           <Route path="/demo" component={Demo} />
@@ -85,7 +114,12 @@ export const Main = function (props) {
             </Route>
             <Route path="/hooks/users" render={() => <h1>hello</h1>} /> */}
           </Route>
-          <Route path="/pure-vs-impure" render={(props) => { return <PureVsImpureDemo customProps="hi" /> }} />
+          <Route
+            path="/pure-vs-impure"
+            render={(props) => {
+              return <PureVsImpureDemo customProps="hi" />;
+            }}
+          />
           <Route path="/router-examples" component={RouterExamples} />
           <Route path="*" component={PageNotFound} />
         </Switch>
