330eff9229a446acd4e4cd8c39874fe9758a0315
Prashant Verma <test_value123>
2021-04-14 13:21:59 +0530

    add getter.

--- src/js/components/content/ScrollToTop.js
+++ src/js/components/content/ScrollToTop.js
@@ -4,13 +4,15 @@ import WithBorder from "../higher-order-components/WithBorder";
  * component to make the page to scroll on the top
  */
 function ScrollToTopOnMount() {
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-    return null;
+  return null;
 }
 
-export default WithBorder(ScrollToTopOnMount,
-    { padding: 10 },
-    'This component will only Scroll the page up.');
+export default WithBorder(
+  ScrollToTopOnMount,
+  { padding: 10 },
+  "This component will only Scroll the page up."
+);
